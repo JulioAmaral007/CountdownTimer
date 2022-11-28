4cb4e1e5b31a1b2a0b9cc1d5580111d5c82ca528
Júlio Cézar <captaingamergroot@gmail.com>
2022-11-27 21:03:40 -0300

    Update Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './global'
+import { Router } from './Router'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
